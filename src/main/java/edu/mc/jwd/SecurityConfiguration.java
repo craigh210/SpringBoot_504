523d0a5d9f8698d70d95873080b618e95dcd02a1
craigh210 <craighunt210@gmail.com>
2020-10-05 15:36:43 -0400

    First update

--- src/main/java/edu/mc/jwd/SecurityConfiguration.java
+++ src/main/java/edu/mc/jwd/SecurityConfiguration.java
@@ -19,7 +19,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated()
+        http.authorizeRequests()
+                .antMatchers("/css/**", "/js/**").permitAll()
+                .anyRequest().authenticated()
                 .and().formLogin();
     }
 
